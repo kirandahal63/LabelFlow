323d7b7ae55c083d1d34d99aa2f236cc0fbe76d5
kirandahal63 <dahal.utsav63@gmail.com>
2026-06-13 20:57:26 +0545

    Complete Login and registration

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -23,6 +23,15 @@ class RegisterSerializer(serializers.Serializer):
 
         return data
 
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            email=validated_data["email"],
+            first_name=validated_data["first_name"],
+            last_name=validated_data["last_name"],
+            password=validated_data["password"]
+        )
+        return user
+
 
 class LoginSerializer(serializers.Serializer):
     email = serializers.EmailField()
--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+from accounts.models import User
+from accounts.serializers import RegisterSerializer
 
-# Create your tests here.
+class AccountsTests(TestCase):
+    def test_create_user(self):
+        user = User.objects.create_user(
+            email="test@example.com",
+            password="testpassword123",
+            first_name="Test",
+            last_name="User"
+        )
+        self.assertEqual(user.email, "test@example.com")
+        self.assertTrue(user.check_password("testpassword123"))
+        self.assertEqual(user.first_name, "Test")
+        self.assertEqual(user.last_name, "User")
+        self.assertTrue(User.objects.filter(email="test@example.com").exists())
+
+    def test_register_serializer(self):
+        data = {
+            "email": "serializer@example.com",
+            "first_name": "Seri",
+            "last_name": "Alizer",
+            "password": "mypassword123",
+            "confirm_password": "mypassword123"
+        }
+        serializer = RegisterSerializer(data=data)
+        self.assertTrue(serializer.is_valid())
+        user = serializer.save()
+        self.assertEqual(user.email, "serializer@example.com")
+        self.assertEqual(user.first_name, "Seri")
+        self.assertEqual(user.last_name, "Alizer")
+        self.assertTrue(User.objects.filter(email="serializer@example.com").exists())
